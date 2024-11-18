8c3837fbedbd37ea4be14f2f9373bdf381826166
陈思 <chensi@chensideMBP.lan>
2024-11-18 01:47:05 -0500

    13

--- test_query_viz.py
+++ test_query_viz.py
@@ -3,8 +3,7 @@ from mylib.query_viz import viz
 
 def test_viz():
     # Absolute path to the CSV file
-    file_path = ("/Users/chensi/Desktop/MIDS/Fall 2024/IDS 706/"
-    "Sizhe_Chen_mini_Project_11/drinks.csv")
+    file_path =  "./drinks.csv"
     save_path = "./average_beer_servings_by_country.png"
     
     # Ensure the CSV file exists
